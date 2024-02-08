baaad9b70ada193eaf78717e88c3bdd61c27c5f8
MathieuFerreira1 <Mathieu.Ferreira@etu.univ-savoie.fr>
2024-02-08 21:07:03 +0100

    add style

--- phpFiles/services.php
+++ phpFiles/services.php
@@ -1,6 +1,6 @@
 <?php
 echo '<h1>Page de Services</h1>';
-echo '<p>Choisissez une option :</p>';
-echo '<a href="vente.php">Vente</a><br>';
-echo '<a href="achat.php">Achat</a>';
+echo '<p>Vous êtes connecté. Vous pouvez accéder à nos services.</p>';
+echo '<p>ici vous pouvez acheter ou louer</p>';
+echo '<a href="login.php"><button>Se déconnecter</button></a>';
 ?><?php
