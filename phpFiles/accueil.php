c4d009ec24147b09c7ff9011a1cbf121d25f6394
MathieuFerreira1 <Mathieu.Ferreira@etu.univ-savoie.fr>
2024-02-08 21:23:16 +0100

    add style and fix login

--- phpFiles/accueil.php
+++ phpFiles/accueil.php
@@ -1,14 +1,12 @@
-<?php
-echo '<!DOCTYPE html>';
-echo '<html lang="en">';
-echo '<head>';
-echo '<meta charset="UTF-8">';
-echo '<link rel="stylesheet" type="text/css" href="accueil.css">';
-echo '</head>';
-echo '<body>';
-echo '<h1>Bienvenue</h1>';
-echo '<p>Choisissez une page à visiter :</p>';
-echo '<a href="login.php">Page de Login</a><br>';
-echo '</body>';
-echo '</html>';
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="..\cssFiles\accueil.css">
+</head>
+<body>
+<h1>Bienvenue</h1>
+<p>Choisissez une page à visiter :</p>
+<a href="login.php">Page de Login</a><br>
+</body>
+</html>
--- phpFiles/services.php
+++ phpFiles/services.php
@@ -1,6 +1,13 @@
-<?php
-echo '<h1>Page de Services</h1>';
-echo '<p>Vous êtes connecté. Vous pouvez accéder à nos services.</p>';
-echo '<p>ici vous pouvez acheter ou louer</p>';
-echo '<a href="login.php"><button>Se déconnecter</button></a>';
-?><?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="..\cssFiles\services.css">
+</head>
+<body>
+<h1>Page de Services</h1>
+<p>Vous êtes connecté. Vous pouvez accéder à nos services.</p>
+<p>Ici vous pouvez acheter ou louer</p>
+<a href="login.php"><button>Se déconnecter</button></a>
+</body>
+</html>
