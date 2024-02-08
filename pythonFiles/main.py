c2a6e4e9edba6412a70004009423175bb48591e5
MathieuFerreira1 <Mathieu.Ferreira@etu.univ-savoie.fr>
2024-02-08 21:55:32 +0100

    add commentaires

--- pythonFiles/main.py
+++ pythonFiles/main.py
@@ -6,10 +6,10 @@ import redis
 r = redis.Redis(host='localhost', port=6379, db=0)
 
 def can_login(user_id):
-    # Obtenez le timestamp actuel
+    # Obtenir le timestamp actuel
     now = time.time()
 
-    # Récupérez toutes les connexions de l'utilisateur
+    # Récupérer toutes les connexions de l'utilisateur
     connections = r.lrange(user_id, 0, -1)
 
     # Initialiser une nouvelle liste vide
@@ -17,14 +17,15 @@ def can_login(user_id):
 
     # Parcourir chaque 'conn' dans 'connections'
     for conn in connections:
-        # Convertir 'conn' en flottant et soustraire le résultat de 'now'
+        # Convertir 'conn' en flottant et soustraire le résultat de 'now' pour obtenir la différence de temps
         time_difference = now - float(conn)
 
-        # Vérifier si le résultat est inférieur ou égal à 600
+        # Vérifier si le résultat est inférieur ou égal à 600, ce qui signifie que la connexion a été effectuée dans les 10 dernières minutes
         if time_difference <= 600:
             # Si la condition est vraie, ajouter 'conn' à 'recent_connections'
             recent_connections.append(conn)
 
+    # Vérifier si 'recent_connections' est inférieur à 10
     if len(recent_connections) < 10:
         # L'utilisateur peut se connecter
         return 1
@@ -32,6 +33,7 @@ def can_login(user_id):
         return 0
 
 def attempt_login(user_id):
+    # Vérifier si l'utilisateur peut se connecter
     can_login_message = can_login(user_id)
     # print(can_login_message)
 
@@ -41,9 +43,13 @@ def attempt_login(user_id):
     return can_login_message
 
 def reset_all_connections(user_ids):
+    # Parcourir chaque 'user_id' dans 'user_ids' et supprimer la liste correspondante
     for user_id in user_ids:
         r.delete(user_id)
 
 if __name__ == "__main__":
+    # récupérer l'identifiant de l'utilisateur à partir des arguments de la ligne de commande
     user_id = sys.argv[1]
+
+    # regarder si l'utilisateur peut se connecter
     print(attempt_login(user_id))
